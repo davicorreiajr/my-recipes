833060813bda73b913abe6e12dbcff00925765ad
davicorreiajr <davicorreiajr@gmail.com>
2020-04-30 12:26:20 -0300

    Give some style to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 
 type Props = {
   className?: string
 }
 
+const Title = styled.h1`
+  color: rgba(0, 0, 0, 0.85);
+  font-family: 'Montserrat', sans-serif;
+  font-size: 42px;
+  font-weight: 600;
+  letter-spacing: 0.03em;
+  text-align: center;
+  margin: 0 0.5em;
+  padding: 0.5em 0;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.25);
+`
+
 const Header = ({ className }: Props) => {
-  return <h1 className={className}>Recipes</h1>
+  return (
+    <div className={className}>
+      <Title>My Recipes</Title>
+    </div>
+  )
 }
 
 export default Header
